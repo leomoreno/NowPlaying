3093a563c2b1bb36ddc2f3d9e62950cd126279ad
Leonardo Moreno <garrik.leonardo@gmail.com>
2015-10-27 17:56:53 -0500

    Navbar directive scaffolding

---
+++ src/app/components/navbar/navbar.directive.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('nowplaying')
+    .directive('acmeNavbar', acmeNavbar);
+
+  /** @ngInject */
+  function acmeNavbar() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/navbar/navbar.html',
+      scope: {
+          cityName: '='
+      },
+      controller: NavbarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function NavbarController() {
+      var vm = this;
+      vm.cityName = vm.cityName;
+    }
+  }
+
+})();
