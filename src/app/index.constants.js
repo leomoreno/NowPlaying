1ed48ae6b3f92dd8a182d496fbe2bf4fb1d0c39b
Leonardo Moreno <garrik.leonardo@gmail.com>
2015-10-29 03:49:55 -0500

    index constants update

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,14 +1,15 @@
-/* global toastr:false, moment:false */
+/* global toastr:false, OAuth:false, moment:false */
 (function() {
   'use strict';
 
   angular
     .module('nowplaying')
     .constant('toastr', toastr)
-    .constant('toastr', toastr)
+    .constant('OAuth', OAuth)
     .constant('tUser', 'dev+biplaying@bunnyinc.com')
     .constant('tUsername', 'BInowplaying')
     .constant('tKey', 'gEFEbGdkVTDfzVgyiiCbzUImi')
+    .constant('oAuthKey', '80l_rz7SK-Vx2N-D938yl2A3FOw')
     .constant('moment', moment);
 
 })();
